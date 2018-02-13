5de16009c070ab5a2b33f94c7d861b165580b8fc
iSteer_Apps <apps@isteer.co.in>
2018-02-13 17:44:15 +0530

    latest code

--- activity/azureiot/activity.go
+++ activity/azureiot/activity.go
@@ -2,7 +2,10 @@ package azureiot
 
 import (
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
 )
+
+var log = logger.GetLogger("activit-azureiot")
 
 // MyActivity is a stub for your Activity implementation
 type MyActivity struct {
@@ -20,9 +23,18 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 }
 
 // Eval implements activity.Activity.Eval
-func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	// do eval
+	sharedAccessKey := context.GetInput("sharedAccessKey").(string)
+	hostName := context.GetInput("hostName").(string)
+	deviceID := context.GetInput("deviceID").(string)
+
+	log.Debug("The hostname is [%s]", hostName)
+	log.Debug("The device is [%s]", deviceID)
+	log.Debug("The shared access key is [%s]", sharedAccessKey)
+
+	context.SetOutput("result", "Trying to conenct to device "+deviceID+" using hostname: "+hostName+"and sharedAccesskey as "+sharedAccessKey)
 
 	return true, nil
 }
